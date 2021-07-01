2739bbe38823a0c585cf08cfe052b588cff5e275
MadMcCrow <perard.git@outlook.com>
2021-07-01 22:07:09 +0200

    :zap: Update project generation

--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
-PathToUE=/run/media/perard/03413941-1a7e-4cfb-9965-2d4264c1fdb5/UE5/UnrealEngine
+PathToUE=$(realpath "../UnrealEngine")
 "$PathToUE/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -projectfiles -project=$(find $(pwd) -iname "*.uproject") -game -rocket -progress -VSCode
