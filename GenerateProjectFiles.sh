57dc0cdc10776147cc8571e560e1bf71587618fb
PERARD-G N <MadMcCrow@users.noreply.github.com>
2021-08-09 17:55:34 +0200

    Update GenerateProjectFiles.sh
    Update Project files generator and comment it

--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/bash
-PathToUE=$(realpath "../UnrealEngine")
-"$PathToUE/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -projectfiles -project=$(find $(pwd) -iname "*.uproject") -game -rocket -progress -VSCode
+# Set Paths for this generator @todo : automate Unreal detection
+UEPath=$(realpath "../UnrealEngine")
+ProjectPath=$(find $(pwd) -iname "*.uproject")
+# Generate VSCode project
+"$UEPath/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -projectfiles -project=$ProjectPath -game -rocket -progress -VSCode
+# Generate CMakelist for Github build (or other IDEs)
+#"$UEPath/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -makefile -project=$ProjectPath -game -engine -progress
