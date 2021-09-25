39afaebd675a03b60ab3d747232bbdd588e67b76
MadMcCrow <perard.git@outlook.com>
2021-09-25 16:12:27 +0200

    :wrench: Improve Project generation

--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
 PathToUE=$(realpath "../UnrealEngine")
-"$PathToUE/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -projectfiles -project=$(find $(pwd) -iname "*.uproject") -game -rocket -progress -VSCode
+#"$PathToUE/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -projectfiles -project=$(find $(pwd) -iname "*.uproject") -game -engine -progress -VSCode -clion
+"$PathToUE/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" -project=$(find $(pwd) -iname "*.uproject") -game -engine -progress -clion -VSCode
